7e7d4bf2e7b541ba9cdffb4f29594f90f5d9bab9
chalosalvador <chalosalvador@gmail.com>
2021-03-12 10:48:10 -0500

    Boilerplate

--- app.config.js
+++ app.config.js
@@ -1,15 +1,15 @@
 import "dotenv/config";
 
 export default {
-  name: "FPB",
+  name: "Grupo Menta - React Native Boilerplate",
   version: "1.0.0",
   extra: {
     apiBaseURL: process.env.REACT_NATIVE_API_BASE_URL,
   },
-  "icon": "./assets/images/icon.png",
-  "splash": {
-    "image": "./assets/images/splash.png",
-    "resizeMode": "cover",
-    "backgroundColor": "#0e4368"
+  icon: "./assets/images/icon.png",
+  splash: {
+    image: "./assets/images/splash.png",
+    resizeMode: "cover",
+    backgroundColor: "#0e4368",
   },
 };
